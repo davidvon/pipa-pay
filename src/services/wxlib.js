e56cf0c0f7f9371f2c99efc6b41aab8773622a3c
davidvon71@163.com <davidvon71@163.com>
2016-07-13 01:19:19 +0800

    修改日志

--- src/services/wxlib.js
+++ src/services/wxlib.js
@@ -1,10 +1,14 @@
+import logger from '../services/log'
+
 export function onMenuShareAppMessage(link, title, desc, logo, callback, errback){
+  logger.log('onMenuShareAppMessage', 'title:' + title)
   wx && wx.onMenuShareAppMessage({
     title: title, // 分享标题
     desc: desc, // 分享描述
     link: link, // 分享链接
     imgUrl: logo, // 分享图标
     success: function () {
+      logger.log('onMenuShareAppMessage', 'success')
       callback && callback()
     },
     cancel: function () {
@@ -32,19 +36,19 @@ export function getOAuthRedirectUrl(appid, url){
   url = encodeURIComponent(url)
   var ret = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+ appid +'&redirect_uri='+ url+
   '&response_type=code&scope=snsapi_base&state=1#wechat_redirect'
-  console.log('[getOAuthRedirectUrl] url:' + ret)
+  logger.log('getOAuthRedirectUrl', 'url:' + ret)
   return ret
 }
 
 export function oAuthCheck(self, code, apibase, appid, redirectUrl, callback){
   if(code){
     self.$http.post(apibase + 'weixin/oauth/decode',{code:code}).then(function (response) {
-      console.log('[oAuthCheck] oauth callback...')
+      logger.log('oAuthCheck', 'oauth callback...')
       callback && callback(response.data)
     })
   } else {
     var oauth_url = getOAuthRedirectUrl(appid, redirectUrl)
-    console.log('[oAuthCheck] oauth url:' + oauth_url)
+    logger.log('oAuthCheck', 'oauth url:' + oauth_url)
     return location.href = oauth_url
   }
 }
