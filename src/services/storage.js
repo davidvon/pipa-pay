acc45a5fcab1fb93bd13ab5eb3938fd1b7850fd6
davidvon71@163.com <davidvon71@163.com>
2016-07-18 20:28:34 +0800

    优化缓存的openid的时间

--- src/services/storage.js
+++ src/services/storage.js
@@ -7,10 +7,15 @@ module.exports = {
     this.wxConfigStatus = true
   },
   set wxOpenId(wxOpenId) {
+    setCookie('PIPA_OPENID', wxOpenId, 360*3)
     localStorage.setItem('PIPA_OPENID', wxOpenId);
   },
   get wxOpenId() {
-    return localStorage.getItem('PIPA_OPENID');
+    var openid = localStorage.getItem('PIPA_OPENID') || getCookie('PIPA_OPENID')
+    if(openid && !localStorage.getItem('PIPA_OPENID')){
+      localStorage.setItem('PIPA_OPENID', openid)
+    }
+    return openid
   },
   clear() {
     localStorage.clear()
